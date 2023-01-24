a736c7da63a8c1952bedbd2c5f20d0ae7e5f8140
Yevhenii Cheremisin <eucheremisin@gmail.com>
2023-01-24 19:20:14 +0200

    update

---
+++ api/gpio/[pinId].js
@@ -0,0 +1,37 @@
+import gpio from 'gpio';
+
+let currentState = {
+  isReady: false,
+  isAlerted: false,
+};
+
+console.log('script started');
+
+const gpio17 = gpio.export(17, {
+  direction: gpio.DIRECTION.IN,
+  interval: 500,
+  ready: function () {
+    console.log('gpio17 is ready');
+    currentState.isReady = true;
+  },
+});
+
+gpio17.on('change', function (val) {
+  // value will report either 1 or 0 (number) when the value changes
+  console.log('move', val);
+});
+
+export default (req, res) => {
+  const {
+    query: { pinId },
+  } = req;
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(
+    JSON.stringify({
+      textMessage: `Pin id passed in query: ${pinId}`,
+      ...currentState,
+    })
+  );
+};
--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-import gpio from 'gpio';
+import fetch from 'node-fetch';
 
-let currentState = {
-  isReady: false,
-  isAlerted: false,
-};
-
-console.log('script started');
-
-const gpio4 = gpio.export(17, {
-  direction: gpio.DIRECTION.IN,
-  interval: 500,
-  ready: function () {
-    console.log('gpio17 is ready');
-    currentState.isReady = true;
-  },
-});
-
-gpio4.on('change', function (val) {
-  // value will report either 1 or 0 (number) when the value changes
-  console.log('move', val);
-});
+setInterval(() => {
+  fetch('/api/gpio/17')
+    .then((response) => response.json())
+    .then((pinData) => {
+      console.log(pinData);
+    });
+}, 200);
