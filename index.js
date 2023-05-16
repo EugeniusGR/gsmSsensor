8df776a3d2fe43dddf11066cf34810055ea11e6f
Yevhenii Cheremisin <eucheremisin@gmail.com>
2023-05-16 22:29:54 +0300

    added temperature gpio

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let currentState = {
 };
 console.log('script started');
 
-const gpio17 = gpio.export(4, {
+const gpio4 = gpio.export(4, {
   direction: gpio.DIRECTION.IN,
   interval: 20,
   ready: function () {
@@ -14,9 +14,23 @@ const gpio17 = gpio.export(4, {
   },
 });
 
-gpio17.on('change', function (val) {
+gpio4.on('change', function (val) {
   // value will report either 1 or 0 (number) when the value changes
   console.log('move', val);
+});
+
+const gpio2 = gpio.export(4, {
+  direction: gpio.DIRECTION.IN,
+  interval: 20,
+  ready: function () {
+    console.log('gpio2 is ready');
+    currentState.isReady = true;
+  },
+});
+
+gpio2.on('change', function (val) {
+  // value will report either 1 or 0 (number) when the value changes
+  console.log('temperature', val);
 });
 
 export default (req, res) => {
