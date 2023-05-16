143bd27537ae5cdf9962449c317af8813ef3bf7d
Yevhenii Cheremisin <eucheremisin@gmail.com>
2023-05-16 21:55:16 +0300

    added core code

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-import fetch from 'node-fetch';
+import gpio from 'gpio';
+let currentState = {
+  isReady: false,
+  isAlerted: false,
+};
+console.log('script started');
 
-setInterval(() => {
-  fetch('/api/gpio/17')
-    .then((response) => response.json())
-    .then((pinData) => {
-      console.log(pinData);
-    });
-}, 200);
+const gpio17 = gpio.export(17, {
+  direction: gpio.DIRECTION.IN,
+  interval: 20,
+  ready: function () {
+    console.log('gpio17 is ready');
+    currentState.isReady = true;
+  },
+});
+gpio17.on('change', function (val) {
+  // value will report either 1 or 0 (number) when the value changes
+  console.log('move', val);
+});
+export default (req, res) => {
+  const {
+    query: { pinId },
+  } = req;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(
+    JSON.stringify({
+      textMessage: `Pin id passed in query: ${pinId}`,
+      ...currentState,
+    })
+  );
+};
