5718f8b7c08ad560429f69cb075394b5354666bf
Yevhenii Cheremisin <eucheremisin@gmail.com>
2023-05-30 21:56:52 +0300

    increased interval

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ gpio4.on('change', function (val) {
 console.log('script started');
 const gpio26 = gpio.export(17, {
   direction: gpio.DIRECTION.IN,
-  interval: 20,
+  interval: 5000,
   ready: function () {
     console.log('gpio17 is ready');
     currentState.isReady = true;
